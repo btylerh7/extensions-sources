cc3f301e6bef4bae5c4e292207cea5567592c810
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2022-03-03 22:32:45 -0500

    last attempt, removed tag parser from get manga details

--- src/MangaGohan/MangaGohanParser.ts
+++ src/MangaGohan/MangaGohanParser.ts
@@ -20,22 +20,22 @@ export const parseMangaDetails = ($: CheerioStatic, mangaId: string): Manga => {
   const author = $('.author-content').find('a').first().text()
   const artist = $('.artist-content').find('a').first().text()
   const desc = $('.Y2IQFc').text()
-  const tags: Tag[] = []
-  const data = $('.sub-menu').find('a')
-  for (const link of data.toArray()) {
-    const id = decodeURI($(link).attr('href')!.split('com/')[1]!)
-    const label = $(link).text().trim()
-    if (!id || !label) continue
-    if (!decodeURI($(link).attr('href')!.split('com/')[1]!)?.startsWith('manga-genre')) continue
-    tags.push({ id: id!, label: label })
-  }
-  const tagSection: TagSection[] = [
-    createTagSection({
-      id: '0',
-      label: 'genres',
-      tags: tags.map((tag) => createTag(tag)),
-    }),
-  ]
+  // const tags: Tag[] = []
+  // const data = $('.sub-menu').find('a')
+  // for (const link of data.toArray()) {
+  //   const id = decodeURI($(link).attr('href')!.split('com/')[1]!)
+  //   const label = $(link).text().trim()
+  //   if (!id || !label) continue
+  //   if (!decodeURI($(link).attr('href')!.split('com/')[1]!)?.startsWith('manga-genre')) continue
+  //   tags.push({ id: id!, label: label })
+  // }
+  // const tagSection: TagSection[] = [
+  //   createTagSection({
+  //     id: '0',
+  //     label: 'genres',
+  //     tags: tags.map((tag) => createTag(tag)),
+  //   }),
+  // ]
 
   return createManga({
     id: mangaId,
@@ -45,7 +45,7 @@ export const parseMangaDetails = ($: CheerioStatic, mangaId: string): Manga => {
     status: status,
     author: author,
     artist: artist,
-    tags: tagSection,
+    // tags: tagSection,
     desc: desc ?? '',
     // hentai
   })
