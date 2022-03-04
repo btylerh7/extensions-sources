0a1c13ad939f5c312d6c1dfdc01c8bcf8c55fa17
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2022-03-03 22:27:40 -0500

    changed es lint indent to 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     'rules': {
         '@typescript-eslint/indent': [
             'error',
-            4
+            2
         ],
         'linebreak-style': [
             'error',
